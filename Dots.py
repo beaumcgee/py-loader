e7344b12164347cc63f78ea7249349a118ebfea7
Beau McGee <beau.mcgee1987@gmail.com>
2022-04-19 21:52:25 -0600

    Added configurable settings
    Added configurable settings to the Spinner and Dot classes. Both classes
    now have configurable delays and dots has a configurable number of dots.
    Both classes also have default values for these settings in the case
    they are not provided.

--- Dots.py
+++ Dots.py
@@ -4,10 +4,11 @@ import time
 import threading
 
 class Dots:
-    def __init__(self, startText, stopText, numDots):
+    def __init__(self, startText, stopText, numDots=20, delay=.1):
         self.startText = startText
         self.stopText = stopText
         self.numDots = numDots
+        self.delay = delay
         self.loading = False
        
         self.thread = threading.Thread(target=self.displaySpinner, args=())
@@ -27,11 +28,11 @@ class Dots:
         count = 0
 
         while self.loading:
-            sys.stdout.write('\033[2K\033[1G')  # clear the line
-            sys.stdout.flush()                  # display cleared line
-            sys.stdout.write(self.startText + ' ' + self.gen_dots(count) + ' ')  # write text and next spinner character to buffer
-            sys.stdout.flush()                  # display text 
-            time.sleep(.1)                      # delay 
+            sys.stdout.write('\033[2K\033[1G')                                      # clear the line
+            sys.stdout.flush()                                                      # display cleared line
+            sys.stdout.write(self.startText + ' ' + self.gen_dots(count) + ' ')     # write text and next spinner character to buffer
+            sys.stdout.flush()                                                      # display text 
+            time.sleep(self.delay)                                                  # delay 
 
             if count < self.numDots:
                 count += 1
--- Spinner.py
+++ Spinner.py
@@ -5,10 +5,11 @@ import threading
 import itertools
 
 class Spinner:
-    def __init__(self, startText, stopText):
+    def __init__(self, startText, stopText, delay=.1):
         self.startText = startText
         self.stopText = stopText
         self.spin = False
+        self.delay = delay 
        
         self.thread = threading.Thread(target=self.displaySpinner, args=())
         
@@ -17,11 +18,11 @@ class Spinner:
         spinner = itertools.cycle(['- ', '\\ ', '| ', '/ '])
 
         while self.spin:
-            sys.stdout.write('\033[2K\033[1G')  # clear the line
-            sys.stdout.flush()                  # display cleared line
+            sys.stdout.write('\033[2K\033[1G')                      # clear the line
+            sys.stdout.flush()                                      # display cleared line
             sys.stdout.write(self.startText + ' ' + next(spinner))  # write text and next spinner character to buffer
-            sys.stdout.flush()                  # display text 
-            time.sleep(.1)                      # delay 
+            sys.stdout.flush()                                      # display text 
+            time.sleep(self.delay)                                  # delay before next spinner character
             
 
     def start(self):
